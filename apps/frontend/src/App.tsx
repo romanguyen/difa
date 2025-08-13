0f87dfd44db531fdcaff4fdda421fb9a6e0cb965
Duong Nguyen Hoang <duong.hoang.nguyen@lifeliqe.com>
2025-08-13 10:17:19 +0200

    Initial commit

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import { Card } from "@difa/ui";
+
+function App() {
+  return (
+    <>
+      <div>
+      <Card/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/ui/src/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface CardProps {
+  title: string;
+  content: string;
+}
+
+export const Card: React.FC<CardProps> = ({ title, content }) => {
+  return (
+    <div className="max-w-sm rounded-lg border overflow-hidden shadow-lg bg-blue-500">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{title}</div>
+        <p className="text-gray-700 text-base">
+          {content}
+        </p>
+      </div>
+    </div>
+  );
+};
