9d908df55e66db3612360fa9a7006ba8a11c632a
Duong Nguyen Hoang <duong.hoang.nguyen@lifeliqe.com>
2025-08-13 14:16:02 +0200

    feat: working frontend and packages

--- packages/ui/src/Card.tsx
+++ packages/ui/src/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 export const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="max-w-sm rounded-lg border overflow-hidden shadow-lg bg-blue-500">
+    <div className="max-w-sm rounded-lg border overflow-hidden shadow-lg bg-amber-500">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base">
